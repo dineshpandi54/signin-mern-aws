da1316d2b5e396b7e2307ede646d5defccb19087
dineshpandi54 <dineshmanoher54@gmail.com>
2025-03-17 17:29:17 +0530

    updating server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors');
 const registerModel = require('./models/User')
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -41,7 +42,14 @@ app.post('/home',(req,res) => {
     .then(registers => res.json(registers))
     .catch(err => res.json(err))
 })
-  
+
+
+
+app.use(express.static(path.join(__dirname, "signup/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "signup/build", "index.html"));
+});
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
